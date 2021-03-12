181187e2e4b3aebab2ec1ab08e7d4244ebf8d2c1
5.010 Nagib System <adrian2536086@yandex.ru>
2021-03-12 16:03:41 +0300

    2.1.3.3.2

---
+++ Device Control 2/Traps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using SnmpSharpNet;
+
+namespace Device_Control_2
+{
+    class Traps
+    {
+	}
+}
